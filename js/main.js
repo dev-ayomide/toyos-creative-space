71105faf6d72959112a7d7d6a4eb1a59321e244d
Omotoyosi Bamigboye <omotoyosi2sure@gmail.com>
2026-06-24 23:21:19 +0100

    mobile-homepage

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
         var email = copyBtn.getAttribute('data-copy');
         navigator.clipboard.writeText(email).then(function () {
           var orig = copyBtn.innerHTML;
-          copyBtn.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 13l4 4L19 7" stroke="#7D52F4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
+          copyBtn.innerHTML = '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 13l4 4L19 7" stroke="#292929" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>';
           setTimeout(function () { copyBtn.innerHTML = orig; }, 1500);
         });
       });
